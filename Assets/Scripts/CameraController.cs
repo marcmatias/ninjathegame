f57a0062edfbca13dfae6397d1d7fca335ce73c2
marcmatias <marcmatias@me.com>
2018-11-02 13:07:06 -0300

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField]
+	private Transform esq, dir;
+	private PlayerController player;
+	
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+	}
+	
+	
+	void Update () {
+		if(player.transform.position.x > esq.position.x && player.transform.position.x < dir.position.x) { 
+			transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float jumpForce;
+	private float moveInput = 0f;
+	private Rigidbody2D rb2d;
+	[SerializeField] private bool isGrounded;
+	[SerializeField] public Transform groundCheck;
+	public float checkRadious;
+	public LayerMask whatIsGround;
+
+	private bool facingRight = true;
+	private int extraJumps;
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .03f;
+	private Vector3 m_Velocity = Vector3.zero;
+	private float runSpeed = 5f;
+	[SerializeField] private Animator animator;
+	[SerializeField] private bool crouch;
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();	
+	}
+	
+	void FixedUpdate(){
+
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadious, whatIsGround);
+		if(crouch == false && animator.GetBool("isAttacking") == false && animator.GetBool("isAttacking2") == false){
+			moveInput = Input.GetAxisRaw("Horizontal") * runSpeed;
+		} else{
+			moveInput = 0f;
+		}
+		animator.SetFloat("Speed", Mathf.Abs(moveInput));
+
+		Vector3 targetVelocity = new Vector2(moveInput, rb2d.velocity.y);
+		rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+		if(facingRight == false && moveInput > 0){
+			Flip();
+		} else if (facingRight == true && moveInput < 0){
+			Flip();
+		}
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
+	}
+
+	void Update () {
+		if(isGrounded == true){
+			extraJumps = 1;
+			animator.SetBool("isJumping", false);
+		}else{
+			animator.SetBool("isJumping", true);
+		}
+		if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0){
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+			extraJumps--;
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow)){
+			crouch = true;
+			animator.SetBool("isCrouching", true);
+		}else if (Input.GetKeyUp(KeyCode.DownArrow)){
+			crouch = false;
+			animator.SetBool("isCrouching", false);
+		}
+		if (Input.GetKeyDown(KeyCode.X)){
+			animator.SetBool("isAttacking", true);
+		}else if (!Input.GetKeyDown(KeyCode.X)){
+			animator.SetBool("isAttacking", false);
+		}
+		if (Input.GetKeyDown(KeyCode.C)){
+			animator.SetBool("isAttacking2", true);
+		}else if (!Input.GetKeyDown(KeyCode.C)){
+			animator.SetBool("isAttacking2", false);
+		}
+	}
+}
